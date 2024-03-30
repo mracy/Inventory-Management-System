5b2da0412386151fbf21f50129222e268f4b9568
Aashish Chaudhary <109914035+mracy@users.noreply.github.com>
2024-03-31 00:26:25 +0545

    put your username and password for server

--- src/dao/ConnectionProvider.java
+++ src/dao/ConnectionProvider.java
@@ -12,7 +12,7 @@ public class ConnectionProvider {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // Establish a connection to the database
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tirupatitradersmanagementsystem", "root", "01234567");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tirupatitradersmanagementsystem", "your_username", "your_password");
             return con;
         } catch (Exception e) {
             // Handle any exceptions that might occur
